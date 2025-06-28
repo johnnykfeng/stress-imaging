ef4c5345e161b95a6a143e0733f4532dea6c04ce
John Feng <johnfengphd@gmail.com>
2025-06-28 14:52:20 -0700

    Add configuration file and update main.py for angle management
    - Introduced a new config.toml file to store serial numbers and angle configurations for Tsinghua and Ramesh setups.
    - Updated main.py to read from the new config file and refactored angle handling to use dictionaries for better organization.
    - Enhanced the move_all_mounts function to iterate through defined steps, improving the control flow for moving rotation mounts.

--- main.py
+++ main.py
@@ -3,51 +3,68 @@ from Devices.camera_automation import CameraAutomation
 from Devices.LED_control import LEDController
 
 import asyncio
-import time
-import os
 from pathlib import Path
 import tomllib
+from utils import rad_to_deg, deg_to_rad
+import numpy as np
+import time
 
-
-
-config_path = Path(__file__).parent / "config" / "rotation_mounts_SN.toml"
+config_path = Path(__file__).parent / "config.toml"
 with open(config_path, "rb") as f:
     config = tomllib.load(f)
 
-
-for key, value in config.items():
-    print(key, value)
-
-analyzer = RotationMount(config["analyzer_SN"])
-qwp1 = RotationMount(config["qwp1_SN"])
-qwp2 = RotationMount(config["qwp2_SN"])
-polarizer = RotationMount(config["polarizer_SN"])
+polarizer = RotationMount(config["polarizer_SN"], label="Polarizer", mirror=False)
+qwp1 = RotationMount(config["qwp1_SN"], label="QWP1", mirror=True)
+qwp2 = RotationMount(config["qwp2_SN"], label="QWP2", mirror=False)
+analyzer = RotationMount(config["analyzer_SN"], label="Analyzer", mirror=True)
 
 rotation_mounts = [analyzer, qwp1, qwp2, polarizer]
 for mount in rotation_mounts:
     mount.open_device()
     mount.setup_conversion()
 
+angles_Tsinghua = {
+    "I1": [5*np.pi/8, None, None, np.pi/8], 
+    "I2": [np.pi/2, None, None, 0], 
+    "I3": [7*np.pi/8, None, None, 3*np.pi/8], 
+    "I4": [3*np.pi/4, None, None, np.pi/4],
+    "I5": [np.pi/2, 3*np.pi/2, 0, np.pi/4],
+    "I6": [np.pi/2, 3*np.pi/2, 0, 3*np.pi/4],
+    "I7": [np.pi/2, 3*np.pi/2, 0, 0],
+    "I8": [np.pi/2, 3*np.pi/2, np.pi/4, np.pi/4],
+    "I9": [np.pi/2, 3*np.pi/2, np.pi/2, np.pi/2],
+   "I10": [np.pi/2, 3*np.pi/2, 3*np.pi/4, 3*np.pi/4],
+    }
+
+angles_Ramesh = {
+    "I1": [0, None, None, np.pi/2], 
+    "I2": [np.pi/4, None, None, np.pi/2], 
+    "I3": [0, None, None, np.pi/4], 
+    "I4": [np.pi/4, None, None, 3*np.pi/4],
+    "I5": [np.pi/2, 3*np.pi/2, np.pi/4, np.pi/2],
+    "I6": [np.pi/2, 3*np.pi/2, np.pi/4, 0],
+    "I7": [np.pi/2, 3*np.pi/2, 0, 0],
+    "I8": [np.pi/2, 3*np.pi/2, np.pi/4, np.pi/4],
+    "I9": [np.pi/2, np.pi/4, 0, 0],
+   "I10": [np.pi/2, np.pi/4, 3*np.pi/4, np.pi/4],
+    }
 
 async def move_mount(mount, position):
     await asyncio.get_event_loop().run_in_executor(None, mount.move_to_position, position)
 
-angles = [90, 90, 180, 180]
 
-async def move_all_mounts():
+async def move_all_mounts(step="I1"):
     tasks = [
-        move_mount(polarizer, angles[0]),
-        move_mount(qwp1, angles[1]),
-        move_mount(qwp2, angles[2]),
-        move_mount(analyzer, angles[3]),
+        move_mount(polarizer, rad_to_deg(angles_Ramesh[step][0])),
+        move_mount(qwp1, rad_to_deg(angles_Ramesh[step][1])),
+        move_mount(qwp2, rad_to_deg(angles_Ramesh[step][2])),
+        move_mount(analyzer, rad_to_deg(angles_Ramesh[step][3])),
     ]
     await asyncio.gather(*tasks)
 
-asyncio.run(move_all_mounts())
+steps = ["I1", "I2", "I3", "I4", "I5", "I6", "I7", "I8", "I9", "I10"]
 
-
-
-
-
-
+for step in steps:
+    asyncio.run(move_all_mounts(step=step))
+    time.sleep(1)
 
